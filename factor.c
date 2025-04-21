94413a939de5d258d84c056973aaabe17bddd3bd
Ajay <muppedaajay@gmail.com>
2025-04-21 22:02:37 +0530

    Added new files and programs

---
+++ factor.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+ int i,n,factor=1;
+ printf("Enter a number:");
+ scanf("%d",&n);
+ for(i=1;i<=n;i++)
+ {
+     factor=factor*i;
+ }
+ printf("factor of %d\n is %d",n,factor);
+    return 0;
+}
